607158785e51bba66ed11eb19c64f9a991b13619
ymarkov <itemruby@gmail.com>
2017-10-03 17:47:38 +0300

    Reactive form create

--- angular-learn/src/app/views/form-1/form-1.component.ts
+++ angular-learn/src/app/views/form-1/form-1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl , Validators, FormArray} from '@angular/forms';
+import {Observable} from 'rxjs/Observable'
 
 @Component({
   selector: 'app-form-1',
@@ -8,16 +9,63 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class Form1Component implements OnInit {
   genders:string [] = ['male', 'female'];
+  forbidenNames = ['Urij', 'Valera'];
   customForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
     this.customForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
-      'gender': new FormControl(null)
+      'userData': new FormGroup({
+        'username': new FormControl( null, [Validators.required, this.checkNameValidator.bind(this)] ),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.checkEmailValidator.bind(this))
+      }),
+      'gender': new FormControl(null),
+      'hobbies': new FormArray([])
+    });
+
+    // this.customForm.valueChanges.subscribe((value)=> {
+    //         console.log(value)
+    // })
+
+    this.customForm.statusChanges.subscribe((value)=> {
+      console.log(value)
     })
+
+
+  }
+
+
+  onSubmit(){
+    console.log(this.customForm)
+  }
+
+  addHobby(){
+      let control = new FormControl(null, Validators.required );
+    (<FormArray>this.customForm.get('hobbies')).push(control)
+  }
+
+  checkNameValidator(control : FormControl): {[s: string] : boolean}{
+
+    if (this.forbidenNames.includes(control.value)){
+       return {'isForbiden': true};
+    }
+    return null;
+  }
+
+  checkEmailValidator(control :FormControl): Observable<any> | Promise<any> {
+
+    let promise = new Promise((resolve, reject)=> {
+      setTimeout(()=>{
+        if(control.value !== 'test@test.ru'){
+          resolve({'validEmail': true})
+        } else {
+          resolve(null)
+        }
+      }, 2000)
+    });
+
+    return promise
   }
 
 }
