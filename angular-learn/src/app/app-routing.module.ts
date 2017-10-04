d544031f6ec3f0edf3be3d77eccfce3774033a36
corvusnigra <itemruby@gmail.com>
2017-10-04 23:48:50 +0300

    add form signup

--- angular-learn/src/app/app-routing.module.ts
+++ angular-learn/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {Form1Component} from "./views/form-1/form-1.component";
+import {SignupComponent} from "./views/signup/signup.component";
 
 
 const routes: Routes = [
     {path: '', redirectTo: '/', pathMatch: 'full'},
-    {path: 'form-1', component: Form1Component}
+    {path: 'form-1', component: Form1Component},
+    {path: 'signup', component: SignupComponent}
 
 ];
 
--- angular-learn/src/app/app.module.ts
+++ angular-learn/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { Form1Component } from './views/form-1/form-1.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { SignupComponent } from './views/signup/signup.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        Form1Component
+        Form1Component,
+        SigninComponent,
+        SignupComponent
     ],
     imports: [
         BrowserModule,
@@ -19,6 +23,7 @@ import { Form1Component } from './views/form-1/form-1.component';
          AppRoutingModule,
         BrowserAnimationsModule,
         MaterializeModule.forRoot(),
+        FormsModule,
         ReactiveFormsModule
     ],
     providers: [],
